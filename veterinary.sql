3d67da5d62ca4e719a5f926653bc77443529f4f1
christine2929 <dechavezchristine0@gmail.com>
2025-12-02 04:14:34 +0800

    feat/insert-10-rows-owners

---
+++ veterinary.sql
@@ -0,0 +1,12 @@
+INSERT INTO owners (ownerid, ofirstname, olastname, address, phone, email)
+VALUES
+(1, 'Christine', 'De Chavez', 'Batangas City', '09171234567', 'christine.dechavez@email.com'),
+(2, 'Angelica', 'Aguba', 'Quezon City', '09182345678', 'angelica.aguba@gmail.com'),
+(3, 'Ahuge', 'De Quina', 'Makati City', '09193456789', 'ahuge.dequina@gmail.com'),
+(4, 'Bianca', 'Corpuz', 'Taguig City', '09204567890', 'bianca.corpuz@gmail.com'),
+(5, 'Irish', 'Fampulme', 'Pasig City', '09215678901', 'irish.fampulme@email.com'),
+(6, 'Francine', 'Feliciano', 'Mandaluyong City', '09226789012', 'francine.feliciano@email.com'),
+(7, 'Jessica', 'Hernandez', 'San Juan City', '09237890123', 'jessica.hernandez@email.com'),
+(8, 'Niur', 'Marasigan', 'Parañaque City', '09248901234', 'niurn@email.com'),
+(9, 'Rafael', 'Ilagan', 'Alabang City', '09259012345', 'rafael.ilagan@email.com'),
+(10, 'Dian', 'Ilagan', 'Alabang City', '09260123456', 'dianne.ilagan@email.com');
